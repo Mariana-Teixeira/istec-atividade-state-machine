6c4b5fa213cce63c0e483679587c5f56360098d0
Mariana Reis Teixeira <teixeira.marianareis@gmail.com>
2026-09-23 17:47:03 +0100

    Initial Godot project

---
+++ Scripts/knight.gd
@@ -0,0 +1,32 @@
+class_name Knight extends CharacterBody2D
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+@export var vision_range: float
+@export var attack_range: float
+@export var speed: float
+
+var state_machine: StateMachine
+var direction_to_cursor: Vector2
+
+func _ready() -> void:
+	pass
+
+func _process(_delta: float) -> void:
+	read_cursor_position()
+
+func _physics_process(delta: float) -> void:
+	pass
+
+func read_cursor_position() -> void:
+	direction_to_cursor = get_global_mouse_position() - global_position
+
+func cursor_within_vision_radius() -> bool:
+	return direction_to_cursor.length() < vision_range
+
+func cursor_within_attack_range() -> bool:
+	return direction_to_cursor.length() < attack_range
+
+func _draw() -> void:
+	draw_arc(Vector2.ZERO, vision_range, 0, TAU, 64, Color.BLUE, 0.5)
+	draw_arc(Vector2.ZERO, attack_range, 0, TAU, 64, Color.RED, 0.5)
